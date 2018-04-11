aca820615d37bc30e80acb6c3e327d338de71359
adwait1-g <adwait.gautham@gmail.com>
2018-04-11 12:42:27 +0530

    Made changes in Page Replacement code

--- PageReplacement/js/index.js
+++ PageReplacement/js/index.js
@@ -1,15 +1,22 @@
-var colors = ["#e040fb", "#ff80ab", "#3f51b5", "#1e88e5", "#009688", "#4caf50", "#cddc39", "#ffeb3b", "#607d8b", "#ff9800"];
+function myfunction() {
+          var cache_size = document.getElementById("csize").value;
+          var req_str = document.getElementById("request_string").value;
+          document.write(cache_size);
+					document.write(req_str);
+          myfunc2();
+}
+
 
 function lru() {
-//	window.alert("in fifo");
-	
+	window.alert("in fifo");
+
 	var no_of_frames , no_of_pages , frames , pages , counter = 0 , time , flag1 , flag2 , i , j , pos , faults = 0;
-	
+
 	var req_str = document.getElementById("request_string");
 	var cache_size = document.getElementById("csize").value();
 
 	if(req_str === "") {
-		window.alert("Empty requests string");		
+		window.alert("Empty requests string");
 		return;
 	}
 	if(isNaN(parseInt(cache_size))) {
@@ -23,12 +30,12 @@ function lru() {
 	pages = req_str.split(" ");
 	no_of_pages = pages.length()
 
-	for(i = 0; i < no_of_frames; i++) 	
-		frames.push('-1');	
+	for(i = 0; i < no_of_frames; i++)
+		frames.push('-1');
 
 	for(i = 0 ; i < no_of_pages; i++) {
 		flag1 = flag2 = 0;
-		
+
 		for(j = 0 ; j < no_of_frames; j++) {
 			if(frames[j] == pages[i]) {
 				counter++;
@@ -43,7 +50,7 @@ function lru() {
 				if(frames[j] == -1) {
 					counter++;
 					faults++;
-					frames[j] = pages[i];	
+					frames[j] = pages[i];
 					time[j] = counter;
 					flag2 = 1;
 					break;
@@ -64,7 +71,7 @@ function lru() {
 
 	return 0;
 }
-		
+
 function findLRU(var time , var n) {
 	var i , minimum = time[0] , pos = 0;
 	for(i = 1; i < n; i++) {
@@ -90,7 +97,7 @@ function optimal() {
                 window.alert("Enter a numeric value for cache size");
                 return;
         }
-	
+
 	no_of_frames = Number(cache_size);
         pages = req_str.split(" ");
         no_of_pages = pages.length()
@@ -98,10 +105,10 @@ function optimal() {
 	for(i = 0 ; i < no_of_frames; i++) {
 		frames.push('-1');
 	}
-	
+
 	for(i = 0 ; i < no_of_pages; i++) {
 		flag1 = flag2 = 0;
-		
+
 		for(j = 0 ; j < no_of_frames; j++) {
 			if(frames[j] == pages[i]) {
 				flag1 = flag2 = 1;
@@ -122,10 +129,10 @@ function optimal() {
 
 		if(flag2 == 0) {
 			flag3 = 0;
-				
+
 			for(j = 0 ; j < no_of_frames; j++) {
 				temp.push('-1');
-				
+
 				for(k = i+1; k < no_of_pages; k++) {
 					if(frames[j] == pages[k]) {
 						temp[j] = k;
@@ -167,11 +174,11 @@ function optimal() {
 
 
 function fifo() {
-	
+
 
 	var req_str = document.getElementById("request_string");
 	var cache_size = document.getElementById("csize").value();
-	  
+
 	          if(req_str === "") {
 	                  window.alert("Empty requests string");
 	                  return;
@@ -180,80 +187,35 @@ function fifo() {
 	                   window.alert("Enter a numeric value for cache size");
 		           return;
 		  }
-	
+
 	var no_of_frames = Number(cache_size);
 	var pages = req_str.split(" ");
 	var no_of_pages = pages.length()
 	var frames;
-	var i , j , k , avail , count = 0;
+	var avail = 0 , count = 0;
 
 	for(i = 0 ; i < no_of_frames; i++)
 		frames.push('-1');
 
-	
-	for(i = 0 ; i < no_of_pages; i++) {
-		avail = 0 ; 
-		
-		for(k = 0 ; k < no_of_frames; k++) {
+
+	for(var i = 0 ; i < no_of_pages; i++) {
+		avail = 0 ;
+
+		for(var k = 0 ; k < no_of_frames; k++) {
 			if(frames[k] == pages[i])
 				avail = 1;
 		}
 
 		if(avail == 0) {
-			frames[j] = pages[i];
-			j = (j+1)%no_of_frames;
+			frames[k] = pages[i];
+			k = (k+1)%no_of_frames;
 			count++;
-
-			//Send the frames data.
 		}
 
+    document.getElementById("result").innerHTML = frames;
 	}
 
-	//Send the count variable(pagr faults).
+	document.getElementById("pge_fault_count").innerHTML = count;
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
