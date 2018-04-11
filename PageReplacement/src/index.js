81b12a5303f72435d658bb4c01deca57aebd5bad
adwait1-g <adwait.gautham@gmail.com>
2018-04-11 16:06:11 +0530

    Fully working page replacement algos.

--- PageReplacement/src/index.js
+++ PageReplacement/src/index.js
@@ -11,83 +11,193 @@ function myfunction() {
 /*This is the fifo() function. */
 
 function fifo() {
-  document.write("this is fifo!!");
+  var no_of_frames = parseInt(cache_size);
+  var pages = requests;
+  var no_of_pages = pages.length;
+  var frames = new Array();
+  var avail = 0;
+  var count = 0;
+
+  for(var i = 0 ; i < no_of_frames; i++)
+    frames[i] = -1;
+
+  for(var i = 0 ; i < no_of_pages; i++)
+    pages[i] = parseInt(pages[i]);
+
+  var j = 0 ;
+
+  for(var i = 0 ; i < no_of_pages; i++) {
+    avail = 0;
+
+    for(var k = 0 ; k < no_of_frames; k++) {
+      if(frames[k] == pages[i])
+        avail = 1;
+    }
+
+    if(avail == 0) {
+      frames[j] = pages[i];
+      j = (j+1)%no_of_frames;
+      count++;
+    }
+
+    document.write(frames + "<br>");
+  }
+    document.write("Page faults = " + count);
+
+  return 0;
 }
 
 /*This is the lru() function.*/
 
 function lru() {
-  window.alert("This is lru.");
+  var no_of_frames = parseInt(cache_size);
+  var pages = requests;
+  var no_of_pages = pages.length;
+  var frames = new Array();
+  var counter = 0;
+  var time = new Array();
+  var flag1 = 0;
+  var flag2 = 0;
+  var pos;
+  var faults = 0;
 
-	var no_of_frames , no_of_pages , frames = new Array() , pages = new Array() , counter = 0 , time = new Array() , flag1 = 0, flag2 = 0, pos , faults = 0;
-
-	no_of_frames = parseInt(cache_size);
-	pages = requests;
-	no_of_pages = pages.length;
-
-
-	for(var i = 0; i < no_of_frames; i++)
-		frames[i] = -1;
+  for(var i = 0 ; i < no_of_frames; i++)
+    frames[i] = -1;
 
   for(var i = 0 ; i < no_of_pages; i++)
     pages[i] = parseInt(pages[i]);
 
+  for(var i = 0; i < no_of_pages; i++) {
+    flag1 = flag2 = 0;
 
-	for(var i = 0 ; i < no_of_pages; i++) {
-		flag1 = flag2 = 0;
-
-		for(var j = 0 ; j < no_of_frames; j++) {
-			if(frames[j] == pages[i]) {
-				counter++;
-				time[j] = counter;
-				flag1 = flag2 = 1;
-				break;
-			}
+    for(var j = 0; j < no_of_frames; j++) {
+      if(frames[j] == pages[i]) {
+        counter++;
+        time[j] = counter;
+        flag1 = flag2 = 1;
+        break;
+      }
     }
 
-		if(flag1 == 0) {
-			for(j = 0 ; j < no_of_frames; j++) {
-				if(frames[j] == -1) {
-					counter++;
-					faults++;
-					frames[j] = pages[i];
-					time[j] = counter;
-					flag2 = 1;
-					break;
-				}
-			}
-		}
 
-		if(flag2 == 0) {
-			pos = findLRU(time , no_of_frames);
-			counter++;
-			faults++;
-			frames[pos] = pages[i];
-			time[pos] = counter;
-		}
+  if(flag1 == 0) {
+    for(var j = 0 ; j < no_of_frames; j++) {
+      if(frames[j] == -1) {
+        counter++;
+        faults++;
+        frames[j] = pages[i];
+        time[j] = counter;
+        flag2 = 1;
+        break;
+      }
+    }
+  }
+  if(flag2 == 0) {
+    var minimum = time[0];
+    var pos = 0;
+    for(var k = 1; k < no_of_frames; k++) {
+      if(time[k] < minimum) {
+        minimum = time[k];
+        pos = k;
+      }
+    }
+      counter++;
+      faults++;
+      frames[pos] = pages[i];
+      time[pos] = counter;
 
-		document.write(frames + "<br>");
-	}
 
-    document.write("page faults = " + faults);
-	return 0;
+  }
+    document.write(frames + "<br>");
+}
+    document.write("Page faults = " + faults);
+
+  return 0;
 }
 
-/*This is a new function.*/
-
-function findLRU(var time , var n) {
-	var minimum = time[0] , pos = 0;
-	for(var i = 1; i < n; i++) {
-		if(time[i[ < minimum) {
-			minimum = time[i];
-			pos = i;
-		}
-	}
-  return pos;
-}
-
-/*This is a new function.*/
 
 function optimal() {
-  window.alert("in optimal()");
+  var no_of_frames = parseInt(cache_size);
+  var pages = requests;
+  var no_of_pages = pages.length;
+  var frames = new Array();
+  var temp = new Array();
+  var flag1 = 0;
+  var flag2 = 0;
+  var flag3 = 0;
+  var pos = 0;
+  var faults = 0;
+  var max = 0;
+
+  for(var i = 0 ; i < no_of_frames; i++)
+    frames[i] = -1;
+
+  for(var i = 0 ; i < no_of_pages; i++)
+    pages[i] = parseInt(pages[i]);
+
+  for(var i = 0 ; i < no_of_pages; i++) {
+    flag1 = flag2 = 0;
+
+    for(var j = 0 ; j < no_of_frames; j++) {
+      if(frames[j] == pages[i]) {
+        flag1 = flag2 = 1;
+        break;
+      }
+    }
+
+    if(flag1 == 0) {
+      for(var j = 0 ; j < no_of_frames; j++) {
+        if(frames[j] == -1) {
+          faults++;
+          frames[j] = pages[i];
+          flag2 = 1;
+          break;
+        }
+      }
+    }
+
+    if(flag2 == 0) {
+      flag3 = 0;
+
+      for(var j = 0 ; j < no_of_frames; j++) {
+        temp[j] = -1;
+
+        for(var k = i + 1; k < no_of_pages; k++) {
+          if(frames[j] == pages[k]) {
+            temp[j] = k;
+            break;
+          }
+        }
+      }
+
+      for(var j = 0 ; j < no_of_frames; j++) {
+        if(temp[j] == -1) {
+          pos = j;
+          flag3 = 1;
+          break;
+        }
+      }
+
+      if(flag3 == 0) {
+        max = temp[0];
+        pos = 0;
+
+        for(var j = 1; j < no_of_frames; j++) {
+          if(temp[j] > max) {
+            max = temp[j];
+            pos = j;
+          }
+        }
+      }
+
+      frames[pos] = pages[i];
+      faults++;
+
+    }
+
+    document.write(frames + "<br>");
+
+  }
+
+  document.write("Page faults = " + faults);
 }
